22f133ce4f49b4d933282a492db6e90d342bd626
Nada <104033293+nody77@users.noreply.github.com>
2024-04-01 16:13:16 +0200

    initial template

---
+++ SkatingTravel/PROBLEM_CLASS.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem
+{
+    public static class PROBLEM_CLASS
+    {
+        #region YOUR CODE IS HERE
+        //Your Code is Here:
+        //==================
+        /// <summary>
+        /// Ali Baba decides to go on a skating travel in the alpine mountain. He has stolen a pair of skis and a trail map listing 
+        /// the mountain’s surfaces and slopes (n in total), and he wants to ski from surface S to surface T where a treasure is exists. 
+        /// </summary>
+        /// <param name="vertices">array of surfaces and their elevations </param>
+        /// <param name="edges">array of trails and their lengths </param>
+        /// <param name="startVertex">name of the start surface to begin from it</param>
+        /// <returns>the minimum valid distance from source “S” to target “T”.</returns>
+        public static int RequiredFunction(Dictionary<string, int> vertices, Dictionary<KeyValuePair<string, string>, int> edges, string startVertex)
+        {
+            //REMOVE THIS LINE BEFORE START CODING
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
